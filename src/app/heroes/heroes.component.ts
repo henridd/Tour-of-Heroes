8b5c7b4c0cdcb281e4e2f4769933fcf5ec5ce60a
Henrique Dalcin Dalmas <henri.Oo@hotmail.com>
2020-08-30 16:26:45 -0300

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Hero } from './../hero';
+import { HEROES } from './../mock-heroes';
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
