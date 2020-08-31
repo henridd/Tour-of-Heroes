c35c1e0f7b9aac318a88b94df4fb2e94e931766f
Henrique Dalcin Dalmas <henri.Oo@hotmail.com>
2020-08-31 15:29:10 -0300

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [				
+  declarations: [					
     AppComponent,
       HeroesComponent,
       HeroDetailComponent,
       MessagesComponent,
+      DashboardComponent
    ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
